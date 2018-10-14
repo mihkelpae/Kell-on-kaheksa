da28a068df4940889f5ee9388de4453bbcfd84b5
Erik Mukk <34135105+erikmukk@users.noreply.github.com>
2018-10-14 10:40:15 +0300

    Muudatused gitignore failis

--- KellOn8/src/main/java/com/example/demo/DemoApplication.java
+++ KellOn8/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,33 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableOAuth2Sso
+public class DemoApplication extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().disable()
+				.antMatcher("/**")
+				.authorizeRequests()
+					.antMatchers("/", "/login")
+					.permitAll()
+				.anyRequest()
+					.authenticated()
+				.and().logout().logoutSuccessUrl("/").permitAll();
+	}
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
