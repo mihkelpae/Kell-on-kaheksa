ebbfc44c85433ab4fa638feadaad358b0070c197
erikmukk <34135105+erikmukk@users.noreply.github.com>
2018-10-19 06:40:29 +0300

    Automatic email sending

--- KellOn8/src/main/java/com/example/demo/controllers/IndexController.java
+++ KellOn8/src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,35 +1,45 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.mail.internet.MimeMessage;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    public JavaMailSender sender;
 
     @RequestMapping(value="/", method = RequestMethod.GET)
     public String home(){
         return "index";
     }
 
-    /*try {
-        sendEmail("email");
-        System.out.println("Email sent!");
-        return "redirect:/";
-    } catch (Exception e) {
-        return "Error while sending email: " + e;
+    @RequestMapping(value="/mailSent", method = RequestMethod.POST)
+    public String sendMail(@RequestParam String address) {
+
+        try {
+            sendEmail(address);
+            System.out.println("Email sent!");
+            return "redirect:/";
+        } catch (Exception e) {
+            return "Error while sending email: " + e;
+        }
     }
 
     private void sendEmail(String recipient) throws Exception{
-        MimeMessage message = sender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-
-        helper.setTo(recipient);
-        helper.setText("This is an automated email from kellon8");
-        helper.setSubject("Hi");
-
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(recipient);
+        message.setSubject("Mail from KellOn8");
+        message.setText("Hi. This is an automatic email from kellon8.");
         sender.send(message);
-    }*/
-
-
+    }
 }
--- KellOn8/src/main/resources/static/javascript/loadMore.js
+++ KellOn8/src/main/resources/static/javascript/loadMore.js
@@ -1,8 +1,10 @@
 function loadMail() {
     var x = document.getElementById("loadMail");
+    var y = document.getElementById("loadMore");
     if (x.style.display === "none") {
         x.style.display = "none";
     } else {
+        y.style.display = "none";
         x.style.display = "block";
     }
 }
